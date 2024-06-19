9f73db41b13b2e53f3cfaa6cac8f324a181fcf28
Tiffany Ni <167052032+tiffanyni@users.noreply.github.com>
2024-06-19 14:30:55 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import WatchedMovieList from "./components/WatchedMovieList";
+import WatchedSummary from "./components/WatchedSummary";
+import Loader from "./components/Loader";
+import MovieDetails from "./components/MovieDetails";
+import MovieList from "./components/MovieList";
+import Box from "./components/Box";
+import Main from "./components/Main";
+import NumResults from "./components/NumResults";
+import Search from "./components/Search";
+import ErrorMessage from "./components/ErrorMessage";
+import NavBar from "./components/NavBar";
+import { useMovies } from "./components/useMovies";
+
+export const KEY = "f5080fa5";
+
+export default function App() {
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return storedValue ? JSON.parse(storedValue) : [];
+  });
+  /*   const [watched, setWatched] = useState([]); */
+  const [query, setQuery] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, isLoading, error } = useMovies(query);
+
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  function handleAddWatched(movie) {
+    setWatched((watched) => [...watched, movie]);
+  }
+
+  function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+  }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
+
+  return (
+    <>
+      <NavBar>
+        <Search query={query} setQuery={setQuery} />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          )}
+          {error && <ErrorMessage message={error} />}
+        </Box>
+        <Box>
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddWatched={handleAddWatched}
+              watched={watched}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList
+                watched={watched}
+                onDeleteWatched={handleDeleteWatched}
+              />
+            </>
+          )}
+        </Box>
+      </Main>
+    </>
+  );
+}
---
+++ src/components/Logo.js
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🎬</span>
+      <h1>FlickJournal</h1>
+    </div>
+  );
+}
