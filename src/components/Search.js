0eebb760d0e3af6a4ebcd2360f392d8d5fff6e85
Tiffany Ni <167052032+tiffanyni@users.noreply.github.com>
2024-06-19 14:13:44 -0400

    Add files via upload

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import { useKey } from "./useKey";
+
+export default function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useKey("Enter", function () {
+    if (document.activeElement === inputElement.current) {
+      return;
+    }
+    inputElement.current.focus();
+    setQuery("");
+  });
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
+    />
+  );
+}
